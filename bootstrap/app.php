6369700f01b88a13e4d87ac655e142f53d6cd7a2
Havennnn <latrellmarable00@gmail.com>
2025-12-30 22:51:28 +0800

    chore: added http header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
+
+Request::setTrustedProxies(
+    ['*'],
+    Request::HEADER_X_FORWARDED_ALL
+);
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
